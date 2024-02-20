871db10c7d20ccb31bef8a71e2ab76199dfe6295
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-02-20 17:06:37 +0100

    Aggiungere i file di progetto.

---
+++ VectorClassOverload/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorClassOverload.Properties;
+
+namespace VectorClassOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = Vector.TryParse("3;5", out v1);
+            Vector v2 = Vector.TryParse("4;6", out v2);
+            double s = 3;
+            Console.WriteLine("{0},{1}", v1.X, v1.Y);
+            Console.WriteLine("{0},{1}", v2.X, v2.Y);
+
+            Console.WriteLine((v1 * v2));
+            Console.WriteLine(v1 + v2);
+            Console.WriteLine(v1 / s);
+            Console.WriteLine(v1 * s);
+            Console.WriteLine(v2 * s);
+            Console.WriteLine(-v2);
+            Console.WriteLine(+v1);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VectorClassOverload/Properties/Vector.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VectorClassOverload.Properties
+{
+    internal class Vector
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Vector(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Vector()
+        {
+
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            Vector v = new Vector(v1.X + v2.X, v1.Y + v2.Y);
+            return v;
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+
+        }
+
+        public static Vector operator -(Vector v)
+        {
+            Vector v1 = new Vector(-v.X, -v.Y);
+            return v1;
+        }
+
+        public static Vector operator +(Vector v)
+        {
+            return v;
+        }
+
+        public static Vector operator *(Vector v1, Vector v2)
+        {
+            Vector v = new Vector(v1.X * v2.X, v1.Y * v2.Y);
+            return v;
+        }
+
+        public static Vector operator *(Vector v1, double s)
+        {
+            Vector v = new Vector(v1.X * s, v1.Y * s);
+            return v;
+        }
+        public static Vector operator *(double s, Vector v1)
+        {
+            return new Vector(v1.X * s, v1.Y * s);
+        }
+
+        public static Vector operator /(Vector v1, double s)
+        {
+            Vector v = v1 * (1 / s);
+            return v;
+        }
+
+        public static Vector Parse(string s)
+        {
+            string[] parts = s.Split(';');
+            Vector v = new Vector(double.Parse(parts[0]), double.Parse(parts[1]));
+            return v;
+        }
+
+
+        public static Vector TryParse(string s, out Vector v)
+        {
+            try
+            {
+                v = Parse(s);
+                return v;
+            }
+            catch
+            {
+                MessageBox.Show("ERRORE");
+                v = null;
+                return v;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{X};{Y}";
+
+        }
+
+
+    }
+}
